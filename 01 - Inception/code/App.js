6c5a19c94dcf813493711719a1ac89f1c89e39e3
Shivananda Sai <shivanandasai.38@gmail.com>
2023-01-19 02:24:27 +0530

    code added for chapter01

---
+++ 01 - Inception/code/App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+  "h1",
+  { style: { color: "blue" } },
+  "Heading"
+);
+const heading2 = React.createElement(
+  "h2",
+  { style: { color: "red" } },
+  "Heading2"
+);
+//   For Nested Elements
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+console.log("🚀 ~ file: index.html:36 ~ heading", heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//   root.render(heading);
+root.render(container);
